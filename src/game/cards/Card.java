088bf1af749566ece24ca8ae17467b8bd39f4d84
Mark Craft <markshocraft@gmail.com>
2016-05-24 20:23:46 -0400

    Cardlist

--- src/game/cards/Card.java
+++ src/game/cards/Card.java
@@ -2,4 +2,13 @@ package game.cards;
 
 public class Card {
 
+	String name;
+	int attack, health;
+	
+	public Card(String name, int attack, int health){
+		this.name = name;
+		this.attack = attack;
+		this.health = health;
+	}
+	
 }
